ed9c8cc94404cc0f442d6edf14ab285f13394308
Laurids33 <laurids.dechow@gmail.com>
2025-06-09 18:49:26 +0200

    Make vehicle move left and right

--- Assets/Scripts/Fahrzeug.cs
+++ Assets/Scripts/Fahrzeug.cs
@@ -4,6 +4,8 @@ public class Fahrzeug : MonoBehaviour
 {
     readonly float drehmomentFaktor = 25000;
     public WheelCollider collVL, collVR, collHL, collHR;
+    readonly float lenkwinkelFaktor = 30;
+    public GameObject radVL, radVR;
 
     void Update()
     {
@@ -12,5 +14,20 @@ public class Fahrzeug : MonoBehaviour
         collVR.motorTorque = drehmoment;
         collHL.motorTorque = drehmoment;
         collHR.motorTorque = drehmoment;
+
+        float lenkwinkel = lenkwinkelFaktor * Input.GetAxis("Horizontal");
+        collVL.steerAngle = lenkwinkel;
+        collVR.steerAngle = lenkwinkel;
+
+        radVL.transform.localEulerAngles = new Vector3(
+            radVL.transform.localEulerAngles.x,
+            collVL.steerAngle,
+            radVL.transform.localEulerAngles.z
+        );
+        radVR.transform.localEulerAngles = new Vector3(
+            radVR.transform.localEulerAngles.x,
+            collVR.steerAngle,
+            radVR.transform.localEulerAngles.z
+        );
     }
 }
